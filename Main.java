9bdd705012926caf357995bf99722d62ccd09059
Mario <m@m.com>
2023-10-09 09:25:24 +0200

    Main prints farewell

--- Main.java
+++ Main.java
@@ -1,4 +1,6 @@
 public static void Main(String[] args) {
 	String salutation = "Hola mundo";
 	System.out.println(salutation);
+	String farewell = "Adiós con el corazón";
+	System.out.println(farewell);
 }
